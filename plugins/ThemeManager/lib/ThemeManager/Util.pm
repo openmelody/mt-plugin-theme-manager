openmelody/mt-plugin-theme-manager

commit f9e828e53590ef9b90f25ea44da79bc176367df2

[#14] use MT::FileMgr to create the theme_thumbs folder if necessary.

plugins/ThemeManager/lib/ThemeManager/Util.pm
@@ -157,8 +157,11 @@ sub _theme_thumb_path {
     my @path = ($app->static_file_path, 'support', 'plugins', $tm->id, 'theme_thumbs');
     my $dest_path = File::Spec->catfile( @path );
     # If the destination directory doesn't exist, we need to create it.
-    if (!-w $dest_path) {
-        mkdir $dest_path;
+    if (!-d $dir) {
+        my $fmgr = MT::FileMgr->new('Local')
+            or return $app->error( MT::FileMgr->errstr );
+        $fmgr->mkpath($dir)
+            or return $app->error( MT::FileMgr->errstr );
     }
     return $dest_path;
 }

plugins/ThemeManager/lib/ThemeManager/Plugin.pm
@@ -729,10 +729,6 @@ sub _make_thumbnail {
         # No screenshot was found, or it's too old--so create one.
         # First, create the destination directory, if necessary.
         my $dir = _theme_thumb_path();
-        if (!-d $dir) {
-            $fmgr->mkpath($dir)
-                or return $app->error( MT::FileMgr->errstr );
-        }
         # Now build and cache the thumbnail URL
         # This is done with thumbalizr.com, a free online screenshot service.
         # Their API is completely http based, so this is all we need to do to

commit a2a979404fe2aa53ce98108b4f5909cfbd12ac5d

Bumping to v0.9.31 upon fixing a syntax error introduced in https://github.com/endevver/mt-plugin-theme-manager/commit/f9e828e53590ef9b90f25ea44da79bc176367df2

plugins/ThemeManager/config.yaml
@@ -4,7 +4,7 @@ key: ThemeManager
 author_link: http://endevver.com/
 author_name: Endevver
 description: 'A comprehensive theme management plugin!'
-version: 0.9.30
+version: 0.9.31
 schema_version: 5
 static_version: 4
 l10n_class: ThemeManager::L10N

plugins/ThemeManager/lib/ThemeManager/Util.pm
@@ -157,12 +157,13 @@ sub _theme_thumb_path {
     my @path = ($app->static_file_path, 'support', 'plugins', $tm->id, 'theme_thumbs');
     my $dest_path = File::Spec->catfile( @path );
     # If the destination directory doesn't exist, we need to create it.
-    if ( ! -d $dir ) {
+    if ( ! -d $dest_path ) {
         # FIXME There are hidden bugs here!!!!  This method returns undef on error but the return value isn't checked for that in most places I found.  It's much better to die from a Util method and use eval in the caller
+        require MT::FileMgr;
         my $fmgr = MT::FileMgr->new('Local')
             or return $app->error( MT::FileMgr->errstr );
-        $fmgr->mkpath($dir)
-            or return $app->error( MT::FileMgr->errstr );
+        $fmgr->mkpath( $dest_path )
+            or return $app->error( $fmgr->errstr );
     }
     return $dest_path;
 }
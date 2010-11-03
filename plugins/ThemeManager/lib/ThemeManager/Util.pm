a2a979404fe2aa53ce98108b4f5909cfbd12ac5d
Jay Allen <jay@endevver.com>
2010-11-03 08:57:56 -0700

    Bumping to v0.9.31 upon fixing a syntax error introduced in https://github.com/endevver/mt-plugin-theme-manager/commit/f9e828e53590ef9b90f25ea44da79bc176367df2

--- plugins/ThemeManager/lib/ThemeManager/Util.pm
+++ plugins/ThemeManager/lib/ThemeManager/Util.pm
@@ -157,12 +157,13 @@ sub _theme_thumb_path {
     my @path = ($app->static_file_path, 'support', 'plugins', $tm->id, 'theme_thumbs');
     my $dest_path = File::Spec->catfile( @path );
     # If the destination directory doesn't exist, we need to create it.
-    if ( ! -d $dir ) {
+    if ( ! -d $dest_path ) {
         # FIXME There are hidden bugs here!!!!  This method returns undef on error but the return value isn't checked for that in most places I found.  It's much better to die from a Util method and use eval in the caller
+        require MT::FileMgr;
         my $fmgr = MT::FileMgr->new('Local')
             or return $app->error( MT::FileMgr->errstr );
-        $fmgr->mkpath($dir)
-            or return $app->error( MT::FileMgr->errstr );
+        $fmgr->mkpath( $dest_path )
+            or return $app->error( $fmgr->errstr );
     }
     return $dest_path;
 }
