44f999e6f040e1faa09e988ee26c2531d60775b3
Jay Allen <jay@endevver.com>
2011-06-24 09:10:45 -0700

    Fixed template build_type regex that wasn't actually a regex.

--- addons/ThemeManager.plugin/lib/ThemeManager/TemplateInstall.pm
+++ addons/ThemeManager.plugin/lib/ThemeManager/TemplateInstall.pm
@@ -580,7 +580,7 @@ sub _parse_build_type {
     my ($type) = @_;
     return $type if ( $type =~ /^[0-4]$/ );
     require MT::PublishOption;
-    if ($type eq 'disable') {
+    if ( $type =~ /^disable/i ) {
         return MT::PublishOption::DISABLED();
     } elsif ($type =~ /^static/i) {
         return MT::PublishOption::ONDEMAND();
