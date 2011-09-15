781caf37f77418e35e87c61395a10f0a7701a1fc
Byrne Reese <byrne@majordojo.com>
2011-09-15 09:43:32 -0700

    Added Link/Unlink to theme options to template list actions.

--- addons/ThemeManager.plugin/lib/ThemeManager/Plugin.pm
+++ addons/ThemeManager.plugin/lib/ThemeManager/Plugin.pm
@@ -1767,6 +1767,30 @@ sub delete_tmpl_backups {
     return $app->load_tmpl('delete_tmpl_backups.mtml', $param);
 }
 
+sub itemset_handler {
+    my ($app,$link) = @_;
+    $app->validate_magic or return;
+    my @ids = $app->param('id');
+    for my $tmpl_id (@ids) {
+        my $tmpl = MT->model('template')->load($tmpl_id) or next;
+        MT->log({
+            blog_id => $tmpl->blog_id,
+            message => ($link eq '*' ? "Linking" : "Unlinking") . " " . $tmpl->name . " to theme file."
+                });
+        $tmpl->linked_file($link);
+        $tmpl->save();
+    }
+    $app->add_return_arg( link_changed => 1 );
+    $app->call_return;
+}
+
+sub itemset_link { 
+    return itemset_handler(@_, '*'); 
+}
+sub itemset_unlink { 
+    return itemset_handler(@_, ''); 
+}
+
 1;
 
 __END__
